12efbfff6004119e2e3ddf6c664309f556b1152d
Brian Whitton <bwhitton@wnyc.org>
2018-05-09 11:45:51 -0400

    no blueprint

--- blueprints/ember-waypoints/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  description: 'Add bower dependencies for waypoints',
-
-  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
-
-  afterInstall: function() {
-    return this.addBowerPackageToProject('waypoints', '~4.0.0');
-  }
-};
